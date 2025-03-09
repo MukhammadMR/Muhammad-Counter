79088e67fcf7e4ae02449b74950cb7034b30a894
Mukhammad <m.makhmudoff@gmail.com>
2025-03-09 13:32:39 +0300

    Базовая версия счетчика: добавлены UILabel и UIButton

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CounterLabelView: UILabel!
+    @IBOutlet weak var tapMeButton: UIButton!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLabel()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTaped(_ sender: Any) {
+        counter += 1
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        CounterLabelView.text = "Значение счетчика: \(counter)"
+    }
+    
 }
 
