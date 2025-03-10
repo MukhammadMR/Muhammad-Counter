7e5fca33c4fa3c054dcbad27b6134b2b5ee414ce
Mukhammad <m.makhmudoff@gmail.com>
2025-03-10 18:54:13 +0300

    Исправлены замечания ревью: улучшена история изменений, запрет редактирования UITextView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -4,16 +4,34 @@
 //
 //  Created by Мухаммад Махмудов on 09.03.2025.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    //MARK: - Outlets
     @IBOutlet weak var historyTextView: UITextView!
     @IBOutlet weak var CounterLabelView: UILabel!
     @IBOutlet weak var increaseButton: UIButton!
     @IBOutlet weak var decreaseButton: UIButton!
     @IBOutlet weak var refreshCounterButton: UIButton!
+    
+    //MARK: - Properties
+    private var counter = 0
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    //MARK: - Setup
+    private func setupUI() {
+        historyTextView.text = "История изменений:"
+        historyTextView.isEditable = false
+        increaseButton.tintColor = .red
+        updateLabel()
+    }
+    
+    //MARK: - Actions
     
     func updateLabel() {
         CounterLabelView.text = "Значение счетчика: \(counter)"
@@ -27,16 +45,6 @@ class ViewController: UIViewController {
         historyTextView.text += "\n[\(timestamp)]: \(text)"
     }
     
-    var counter = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        historyTextView.text = "История изменений:"
-        historyTextView.isEditable = false
-        increaseButton.tintColor = .red
-        updateLabel()
-    }
-
     @IBAction func increaseButtonTaped(_ sender: Any) {
         counter += 1
         updateLabel()
@@ -45,9 +53,9 @@ class ViewController: UIViewController {
     
     @IBAction func decreaseButtonTapped(_ sender: Any) {
         if counter > 0 {
-        counter -= 1
-        updateLabel()
-        addToHistory("Значение изменено на -1")
+            counter -= 1
+            updateLabel()
+            addToHistory("Значение изменено на -1")
         } else {
             addToHistory("Попытка уменьшить значение счётчика ниже 0")
         }
